6287998e83b231dacfd8c73ab83199a56f460d34
Lebre Marianne <mlebre@bs406-s16-12.insa-lyon.fr>
2015-01-05 18:01:07 +0100

    Test for population

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include "population.h"
 #include "prey.h"
 #include "predator.h"
+#include "bwindow.h"
+#include "agent.h"
 
 //===================================================================
 //                 Declare Miscelaneous Functions
@@ -19,6 +21,15 @@
 int main()
 {
   printf("Hello world!!\n");
+
+  //Création d'une population
+  population Boids=population();
+  Boids.Set_W(200);
+  Boids.Set_H(200);
+  Boids.Set_size(10);
+  
+
+
   return 0;
 } 
 
